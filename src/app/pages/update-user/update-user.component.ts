e90a36f8448395d2637d291082120ce1491c6611
negri234279 <rafalejo@live.com>
2023-03-02 23:21:06 +0100

    ✨ Add update user

--- src/app/pages/update-user/update-user.component.ts
+++ src/app/pages/update-user/update-user.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, OnDestroy } from '@angular/core'
-import { ActivatedRoute } from '@angular/router'
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
+import { ActivatedRoute, Router } from '@angular/router'
+import { Subscription } from 'rxjs'
+import { User, UserRoles } from 'src/app/models/User'
+import { UserService } from 'src/app/services/user-service.service'
 
 @Component({
     selector: 'app-update-user',
@@ -7,19 +11,56 @@ import { ActivatedRoute } from '@angular/router'
 })
 export class UpdateUserComponent implements OnInit, OnDestroy {
     public id!: string
-    private sub: any
+    public user!: User
+    public isLoading = false
+    public form: FormGroup
+    private sub!: Subscription
+    public roles: string[] = Object.values(UserRoles)
+    public role: string = ''
 
-    constructor(private route: ActivatedRoute) {}
+    constructor(
+        private userService: UserService,
+        private route: ActivatedRoute,
+        private router: Router,
+        private formBuilder: FormBuilder
+    ) {
+        this.form = this.formBuilder.group({
+            nick: new FormControl(['', Validators.required]),
+            fullname: new FormControl(['', Validators.required]),
+            active: new FormControl([false, Validators.required]),
+            role: new FormControl(['', Validators.required]),
+        })
+    }
 
     ngOnInit() {
         this.sub = this.route.params.subscribe((params) => {
-            this.id = params['id']
-
-            // In a real app: dispatch action to load the details here.
+            const id = params['id']
+            this.getUser(id)
         })
     }
 
     ngOnDestroy() {
         this.sub.unsubscribe()
+    }
+
+    getUser(id: string) {
+        this.isLoading = true
+        this.userService.getUser(id).subscribe((user) => {
+            this.user = user
+            this.role = user.role
+            this.form.patchValue(user)
+            this.isLoading = false
+        })
+    }
+
+    submitForm() {
+        const updatedUser = { ...this.user, ...this.form.value }
+        this.userService.updateUser(updatedUser).subscribe(() => {
+            this.router.navigate(['/'])
+        })
+    }
+
+    cancel() {
+        this.router.navigate(['/'])
     }
 }
