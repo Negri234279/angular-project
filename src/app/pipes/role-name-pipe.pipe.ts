d01b3a7c572bbe79c0d157664e28c86d2ed8ac00
negri234279 <rafalejo@live.com>
2023-03-03 15:30:11 +0100

    Update RoleNamePipePipe

--- src/app/pipes/role-name-pipe.pipe.ts
+++ src/app/pipes/role-name-pipe.pipe.ts
@@ -1,16 +1,24 @@
 import { Pipe, PipeTransform } from '@angular/core'
+import { UserRoles } from '../models/User'
+
+// type RoleMap = Record<UserRoles, string>
+
+interface RoleMap {
+    [i: string]: string
+}
 
 @Pipe({
     name: 'roleNamePipe',
 })
 export class RoleNamePipePipe implements PipeTransform {
+    private roleMap: RoleMap = {
+        [UserRoles.Student]: 'student',
+        [UserRoles.Teacher]: 'teacher',
+        // [UserRoles.Other]: 'other',
+    }
+
     transform(value: string): string {
-        if (value.toLowerCase() === 'student') {
-            return 'student'
-        } else if (value.toLowerCase() === 'teacher') {
-            return 'teacher'
-        } else {
-            return 'other'
-        }
+        const normalizedValue = value.toLowerCase()
+        return this.roleMap[normalizedValue] || 'other'
     }
 }
