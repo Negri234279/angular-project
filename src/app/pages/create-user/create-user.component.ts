bbac1f990a45ab898fed5b8a899bf49c733a4262
negri234279 <rafalejo@live.com>
2023-03-08 00:24:37 +0100

    🚚 🩹 Fix submit name and rename form variable

--- src/app/pages/create-user/create-user.component.ts
+++ src/app/pages/create-user/create-user.component.ts
@@ -3,16 +3,19 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 import { Router } from '@angular/router'
 import { User, UserRoles } from 'src/app/models/User'
 import { UserService } from 'src/app/services/user-service.service'
-import { fullnameValidation, nickValidation, roleValidation } from 'src/app/validations/user.validation'
+import {
+    fullnameValidation,
+    nickValidation,
+    roleValidation,
+} from 'src/app/validations/user.validation'
 import { v4 as uuidv4 } from 'uuid'
-
 
 @Component({
     selector: 'app-create-user',
     templateUrl: './create-user.component.html',
 })
 export class CreateUserComponent implements OnDestroy {
-    newUser: FormGroup
+    form: FormGroup
     userRoles = UserRoles
 
     constructor(
@@ -20,7 +23,7 @@ export class CreateUserComponent implements OnDestroy {
         private userService: UserService,
         private router: Router
     ) {
-        this.newUser = this.frmBuilder.group({
+        this.form = this.frmBuilder.group({
             nick: nickValidation,
             fullname: fullnameValidation,
             role: roleValidation,
@@ -33,10 +36,10 @@ export class CreateUserComponent implements OnDestroy {
         const uuid = uuidv4()
         const user = new User(
             uuid,
-            this.newUser.value.nick,
-            this.newUser.value.fullname,
+            this.form.value.nick,
+            this.form.value.fullname,
             true,
-            this.newUser.value.role
+            this.form.value.role
         )
 
         this.userService.addUser(user).subscribe(() => {
@@ -49,6 +52,6 @@ export class CreateUserComponent implements OnDestroy {
     }
 
     ngOnDestroy() {
-        this.newUser.reset()
+        this.form.reset()
     }
 }
